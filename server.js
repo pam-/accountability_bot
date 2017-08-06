f89d5fe5888d0281457cf1a825f908deb5c591b9
pam- <pam.assogba@gmail.com>
2017-08-06 19:27:57 -0400

    removed mongo and tweaked server.js

--- server.js
+++ server.js
@@ -6,7 +6,6 @@ var fs = require('fs'),
     express = require('express'),
     chalk = require('chalk'),
     sass = require('node-sass-middleware'),
-    mongoose = require('mongoose'),
     app = express(),
     Twit = require('twit'),
     config = {
@@ -19,18 +18,6 @@ var fs = require('fs'),
       }
     },
     T = new Twit(config.twitter),
-    stream = T.stream('statuses/sample');
-
-/**
- * Connect to MongoDB.
- */
-mongoose.Promise = global.Promise;
-mongoose.connect(process.env.MONGODB_URI || process.env.MONGOLAB_URI);
-mongoose.connection.on('error', (err) => {
-  console.error(err);
-  console.log('%s MongoDB connection error. Please make sure MongoDB is running.', chalk.red('✗'));
-  process.exit();
-});
 
 app.set('port', process.env.PORT || 3000);
 app.use(express.static('public'));
@@ -45,25 +32,22 @@ var TwitBot = TwitBot || {
   friday_message: "Yo! How did you do on your thing?",
   today: () => {
     var today = new Date().getDay()
-    if (today === 2) {
+    if (today === 1) {
       return 'monday';
     } else if (today === 6) {
       return 'friday'
     }
   },
 
-  tweet: (message) => {
+  tweet: (message, user) => {
     T.post('statuses/update', {
       status: '@' + user + ' ' + message
     }, function(err, data, response) {
       if (err){
-           // TODO: Proper error handling?
-        console.log('Error!');
         console.log(err);
       }
       else{
-        // did it work
-        console.log(data)
+        console.log(`It worked! Here's the data: ${response}`)
       }
     });
   },
@@ -79,12 +63,11 @@ var TwitBot = TwitBot || {
           });
 
           users.forEach(user => {
-            console.log(user)
-            console.log(TwitBot.today())
-            if (TwitBot.today === 'monday') {
-              TwitBot.tweet(monday_message);
-            } else if (TwitBot.today === 'friday') {
-              TwitBot.tweet(friday_message);
+            var day = TwitBot.today()
+            if (day === 'monday') {
+              TwitBot.tweet(TwitBot.monday_message, user);
+            } else if (day === 'friday') {
+              TwitBot.tweet(TwitBot.friday_message, user);
             };
           });
         }
@@ -95,123 +78,6 @@ var TwitBot = TwitBot || {
 };
 
 TwitBot.init();
-
-// T.get('followers/list', { screen_name: process.env.TWITTER_HANDLE },  function (err, data, response) {
-//   if (err) {
-//     console.log(err);
-//   } else {
-//     console.log('this is the data', data)
-//     if (data.users.length > 0) {
-//       var users = data.users.map(user => {
-//         return user.screen_name;
-//       }),
-
-//       users.forEach(user => {
-//         console.log(user)
-
-//         T.post('statuses/update', {
-//           status: '@' + user + ' ' + random_from_array(bot_responses),
-//           in_reply_to_status_id: status.id_str
-//         }, function(err, data, response) {
-//           if (err){
-//               /* TODO: Proper error handling? */
-//             console.log('Error!');
-//             console.log(err);
-//           }
-//           else{
-//             //
-//           }
-//         });
-//       });
-//     }
-//   }
-// })
-
-/* You can use uptimerobot.com or a similar site to hit your /tweet endpoint to wake up your app and make your Twitter bot tweet. */
-
-app.use("/tweet", function (request, response) {
-  console.log('here')
-
-  /* Respond to @ mentions */
-  // fs.readFile(__dirname + '/last_mention_id.txt', 'utf8', function (err, last_mention_id) {
-  //   /* First, let's load the ID of the last tweet we responded to. */
-  //   console.log('last_mention_id:', last_mention_id);
-
-
-  //   T.get('search/tweets', { q: 'to:' + process.env.TWITTER_HANDLE + ' -from:' + process.env.TWITTER_HANDLE, since_id: last_mention_id }, function(err, data, response) {
-  //     /* Next, let's search for Tweets that mention our bot, starting after the last mention we responded to. */
-  //     if (data.statuses.length){
-  //       // console.log(data.statuses);
-  //       data.statuses.forEach(function(status) {
-  //         console.log(status.id_str);
-  //         console.log(status.text);
-  //         console.log(status.user.screen_name);
-
-  //         /* Now we can respond to each tweet. */
-  //         T.post('statuses/update', {
-  //           status: '@' + status.user.screen_name + ' ' + random_from_array(bot_responses),
-  //           in_reply_to_status_id: status.id_str
-  //         }, function(err, data, response) {
-  //           if (err){
-  //               /* TODO: Proper error handling? */
-  //             console.log('Error!');
-  //             console.log(err);
-  //           }
-  //           else{
-  //             fs.writeFile(__dirname + '/last_mention_id.txt', status.id_str, function (err) {
-  //               /* TODO: Error handling? */
-  //             });
-  //           }
-  //         });
-  //       });
-  //     } else {
-  //        No new mentions since the last time we checked. 
-  //       console.log('No new mentions...');
-  //     }
-  //   });
-  // });
-
-  // /* Respond to DMs */
-
-  // fs.readFile(__dirname + '/last_dm_id.txt', 'utf8', function (err, last_dm_id) {
-  //   /* Load the ID of the last DM we responded to. */
-  //   console.log('last_dm_id:', last_dm_id);
-
-  //   T.get('direct_messages', { since_id: last_dm_id, count: 200 }, function(err, dms, response) {
-  //     /* Next, let's search for Tweets that mention our bot, starting after the last mention we responded to. */
-  //     if (dms.length){
-  //       dms.forEach(function(dm) {
-  //         console.log(dm.sender_id);
-  //         console.log(dm.id_str);
-  //         console.log(dm.text);
-
-  //         /* Now we can respond to each tweet. */
-  //         T.post('direct_messages/new', {
-  //           user_id: dm.sender_id,
-  //           text: random_from_array(bot_responses)
-  //         }, function(err, data, response) {
-  //           if (err){
-  //             /* TODO: Proper error handling? */
-  //             console.log('Error!');
-  //             console.log(err);
-  //           }
-  //           else{
-  //             fs.writeFile(__dirname + '/last_dm_id.txt', dm.id_str, function (err) {
-  //               /* TODO: Error handling? */
-  //             });
-  //           }
-  //         });
-  //       });
-  //     } else {
-  //       /* No new DMs since the last time we checked. */
-  //       console.log('No new DMs...');
-  //     }
-  //   });
-  // });
-
-  /* TODO: Handle proper responses based on whether the tweets succeed, using Promises. For now, let's just return a success message no matter what. */
-  // response.sendStatus(200);
-});
 
 /**
  * Start Express server.
